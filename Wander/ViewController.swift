309ab15855f1c3834b13e506317df517120635d9
Siddharth Garg <siddharth.garg85@gmail.com>
2015-02-22 03:29:47 +0530

    Initial Commit

---
+++ Wander/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wander
+//
+//  Created by Siddharth Garg on 22/02/15.
+//  Copyright (c) 2015 All Those who Wander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WanderTests/WanderTests.swift
@@ -0,0 +1,36 @@
+//
+//  WanderTests.swift
+//  WanderTests
+//
+//  Created by Siddharth Garg on 22/02/15.
+//  Copyright (c) 2015 All Those who Wander. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WanderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
