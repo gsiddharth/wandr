68222233648d602c626e22a82181e4eda3869e00
Siddharth Garg <siddharth.garg85@gmail.com>
2015-02-22 15:50:05 +0530

    login logic 1

---
+++ Wander/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  Wander
+//
+//  Created by Siddharth Garg on 22/02/15.
+//  Copyright (c) 2015 All Those who Wander. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        
+        var username = "sid"
+        var password = "sid"
+        
+        if identifier == "LoginSuccessSegue" {
+            if self.usernameTextField.text == username && self.passwordTextField.text == password {
+                return true
+            } else {
+                self.statusLabel.text = "Incorrect username or password"
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ Wander/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  Wander
+//
+//  Created by Siddharth Garg on 22/02/15.
+//  Copyright (c) 2015 All Those who Wander. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
